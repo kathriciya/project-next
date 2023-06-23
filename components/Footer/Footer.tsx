a8d61d78b5363cb9b803f8aa5a06613e49156387
Ekaterina <Kata_Yurevna@mail.ru>
2023-06-24 04:04:16 +0700

    create Footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import cls from './footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={`container ${cls.footer}`}>
+      Created by &copy;kathriciya
+    </footer>
+  );
+};
+
+export default Footer;
