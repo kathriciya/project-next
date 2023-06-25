577f7b6ae2129718a808d0526d57f121fed43b94
Ekaterina <Kata_Yurevna@mail.ru>
2023-06-26 02:07:04 +0700

    update blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,38 +1,25 @@
-import { Metadata } from 'next';
-import Link from 'next/link';
-// import Icon from './../../public/rocket.svg';
+'use client';
+
+import PostSearch from '@/components/PostSearch/PostSearch';
+import Posts from '@/components/Posts/Posts';
+import { getAllPosts } from '@/services/getPosts';
+import { useEffect, useState } from 'react';
 import cls from './blog.module.css';
 
-async function getData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    next: {
-      revalidate: 60,
-    },
-  });
+export default function Blog() {
+  const [posts, setPosts] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  if (!response.ok) throw new Error('Something went wrong!');
-  return response.json();
-}
-
-export const metadata: Metadata = {
-  title: 'Blog | Next App',
-};
-
-export default async function Blog() {
-  const posts = await getData();
-
+  useEffect(() => {
+    getAllPosts()
+      .then(setPosts)
+      .finally(() => setLoading(false));
+  }, []);
   return (
     <>
-      <h1>Blog</h1>
-      <ul className={cls.listPost}>
-        {posts.map((post: any) => (
-          <li key={post.id}>
-            <Link href={`/blog/${post.id}`} className={cls.linkPost}>
-              {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <h1 className={cls.title}>Blog</h1>
+      <PostSearch onSearch={setPosts} />
+      {loading ? <h3>Loading ...</h3> : <Posts posts={posts} />}
     </>
   );
 }
